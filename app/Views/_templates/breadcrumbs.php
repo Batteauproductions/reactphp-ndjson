36d989f8354cfcc8d97f983b559b198a06040fa8
Dunderholm <batteauproductions@gmail.com>
2025-06-12 22:53:16 +0200

    split styling files

--- app/Views/_templates/breadcrumbs.php
+++ app/Views/_templates/breadcrumbs.php
@@ -1,16 +1,18 @@
 <nav aria-label="breadcrumb">
-    <ul class="breadcrumbs">
-        <li>Home</li>
-        <?php foreach ($breadcrumbs as $crumb): ?>
-            <?php if (!empty($crumb['url'])): ?>
-                <li>
-                    <?= esc($crumb['label']) ?>
-                </li>
-            <?php else: ?>
-                <li aria-current="page">
-                    <?= esc($crumb['label']) ?>
-                </li>
-            <?php endif; ?>
-        <?php endforeach; ?>
-    </ul>
+    <div class="grid-container">
+        <ul class="breadcrumbs">
+            <li><a href="<?= base_url('home'); ?>"><i class="fa-solid fa-house"></i></a></li>
+            <?php foreach ($breadcrumbs as $crumb): ?>
+                <?php if (!empty($crumb['url'])): ?>
+                    <li>
+                        <?= esc($crumb['label']) ?>
+                    </li>
+                <?php else: ?>
+                    <li aria-current="page">
+                        <?= esc($crumb['label']) ?>
+                    </li>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </ul>
+    </div>
 </nav>
