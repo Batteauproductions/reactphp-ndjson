7f8cff6aa4e94532bc9cc1cca0b22ff1a0959295
Dunderholm <batteauproductions@gmail.com>
2025-07-14 19:59:38 +0200

    redesign choice template

--- app/Views/character/parts/modal/Choice.php
+++ app/Views/character/parts/modal/Choice.php
@@ -1,32 +1,33 @@
 <!-- MODAL FOR MAKING CHOICES -->
-<div class="reveal large" id="selection-modal" data-reveal>
+<div class="reveal" id="selection-modal" data-reveal>
     
     <div data-id="modal-loading" class="text-center">
         <i class="fa-solid fa-spinner fa-spin"></i> Gegevens laden
     </div>
 
     <form id="modal-form" class="grid-x grid-padding-x grid-padding-y" style="display:none;">
-        <div id="selection-modal-dropdowns" class="cell small-12 medium-12 large-3">
-            <div class="grid-x">
-                <label id="lbl-type" for="type" class="cell" style="display:none;">Type
+        <div id="selection-modal-dropdowns" class="cell small-12">
+            <div class="grid-x grid-margin-x align-center">
+                <label id="lbl-type" for="type" class="cell small-12 medium-4 large-2 text-center" style="display:none;">Type
                     <select id="type" name="type" class="chosen-select">
                         <!-- dynamic filled -->
                     </select>
                 </label>
-                <label id="lbl-subtype" for="subtype" class="cell" style="display:none;">Sub-type
+                <label id="lbl-subtype" for="subtype" class="cell small-12 medium-4 large-2 shrink text-center" style="display:none;">Sub-type
                     <select id="subtype" name="subtype" class="chosen-select">
                         <!-- dynamic filled -->
                     </select>
                 </label>
-                <label id="lbl-amount" for="amount" class="cell" style="display:none;">
+                <label id="lbl-amount" for="amount" class="cell small-12 medium-4 large-2 shrink text-center" style="display:none;">
                     Hoeveelheid
                     <input id="amount" type="number" name="amount" value="1" />
                 </label>
             </div>
+            <hr>
         </div>
-        <div id="choice-image-container" class="cell small-12 medium-6 large-3" style="display:none;">
+        <div id="choice-image-container" class="cell small-12 medium-6 large-4" style="display:none;">
             <img id="choice-image" src="" alt="" />
-        </div>        
+        </div>     
         <div class="cell auto">
             <section id="choice-description">
                 <!-- dynamic filled -->
@@ -38,6 +39,7 @@
                 <!-- dynamic filled -->
             </section>
         </div>
+           
     </form>
     
     <button class="close-button" data-close aria-label="Close modal" type="button">
